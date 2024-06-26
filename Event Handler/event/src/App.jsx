d9c5cf16ebaa902c3edf890e9db67c2f491fe998
kirti berekar <berekarkirti11@gmail.com>
2024-06-26 22:32:44 +0530

    event handler

---
+++ Event Handler/event/src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css';
+import { useState } from 'react';
+import './components/post.css'
+
+function App() {
+  const [posts, setPosts] = useState([]);
+
+  const fetchData = () => {
+    fetch(`https://jsonplaceholder.typicode.com/posts`)
+      .then((res) => res.json())
+      .then((data) => setPosts(data))
+      .catch((err) => console.error(err));
+  };
+
+  return (
+    <div className="App">
+      <button onClick={fetchData}>GET POSTS</button>
+      <hr />
+      {posts.map((post) => (
+        <div className="post" key={post.id} style={{}}>
+          <h2>{post.title}</h2>
+          <p>{post.body}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;

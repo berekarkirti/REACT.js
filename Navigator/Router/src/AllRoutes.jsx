f68622649728a2984548f25ffb752cb373a79a1a
kirti berekar <berekarkirti11@gmail.com>
2024-07-20 18:42:19 +0530

    Router

--- Navigator/Router/src/AllRoutes.jsx
+++ Navigator/Router/src/AllRoutes.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Components/Home';
 import About from './Components/About';
 import NotFound from './Components/NotFound';
 import Login from './Components/Login';
 import ProductDetail from './Components/ProductDetail';
 import Product from './Components/Product';
+import PrivateRoute from './Components/PrivateRoute';
 
 
 const AllRoutes = () => {
@@ -13,11 +14,25 @@ const AllRoutes = () => {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
-      <Route path="/product" element={<Product />} />
-      <Route path="/products/:id" element={<ProductDetail />} />
       <Route path="/login" element={<Login />} />
-      <Route path="*" element={<NotFound />} />
+      <Route
+        path="/product"
+        element={
+          <PrivateRoute>
+            <Product />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/products/:id"
+        element={
+          <PrivateRoute>
+            <ProductDetail />
+          </PrivateRoute>
+        }
+      />
     </Routes>
+
   );
 };
 
